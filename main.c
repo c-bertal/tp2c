bb1fdc91d726c4af0d4f2c814f953093ce211ff2
c-bertal <hubic34@laposte.net>
2020-04-14 10:07:31 +0200

    tkt

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/*
+ *   x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 = 52
+ *   x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 = 57
+ *   x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 = 62
+ *   x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 = 67
+ *   x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 +x14 = 72
+ *
+ *   x E {2, 3, 5, 7, 9}
+ *   n E { 10, 11, 12 ,13, 14}
+ *
+ */
+
+
+int main(int argc, char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
